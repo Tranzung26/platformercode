42fe6311854eb6d5deaaebb1b36722707dc0c08b
lequangtrieu <trieulqde160447@gmail.com>
2024-10-22 18:18:48 +0700

    update source

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // tao bien luu tru audioSource
+    public AudioSource musicAudioSource;
+	public AudioSource sfxAudioSource;
+
+	// tao bien luu tru audio Clip
+	public AudioClip musicClip;
+	public AudioClip coinClip;
+	// Start is called before the first frame update
+	void Start()
+    {
+        musicAudioSource.clip = musicClip;
+		musicAudioSource.Play();
+    }
+
+	public void PlaySFX(AudioClip sfxClip)
+	{
+		sfxAudioSource.clip = sfxClip;
+		sfxAudioSource.PlayOneShot(sfxClip);
+	}
+
+}
