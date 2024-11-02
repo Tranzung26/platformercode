d806059ca2f73357dbbd8db4e47227d5160f438a
lequangtrieu <trieulqde160447@gmail.com>
2024-11-02 15:51:24 +0700

    update source

--- Assets/Scripts/TrieuLQScript/HandleCoinCollision.cs
+++ Assets/Scripts/TrieuLQScript/HandleCoinCollision.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-public class HandleCollision : MonoBehaviour
+public class HandleCoinCollision : MonoBehaviour
 {
     public int Coin = 0;
     public TextMeshProUGUI CoinText;
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-		CoinText.SetText(Coin.ToString());
-	}
+        Coin = PlayerPrefs.GetInt("saveCoin");
+        CoinText.SetText(PlayerPrefs.GetInt("saveCoin").ToString());
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,7 +30,9 @@ public class HandleCollision : MonoBehaviour
     {
 	    if (collision.CompareTag("CoinTag"))
 	    {
-		    Coin++;
+            audioManager.PlaySFX(audioManager.coinClip);
+            Coin++;
+            //PlayerPrefs.SetInt("saveCoin", Coin); // check if here
             CoinText.SetText(Coin.ToString());
             Destroy(collision.gameObject);
 	    }
--- Assets/Scripts/TrieuLQScript/HandleVictory.cs
+++ Assets/Scripts/TrieuLQScript/HandleVictory.cs
@@ -1,14 +1,20 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HandleVictory : MonoBehaviour
 {
     public GameObject gameWinUI;
+    private bool isGameWon = false;
+    private HandleCoinCollision handleCoinCollision;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGameWon = false;
+        // Tìm component HandleCollision trong scene
+        handleCoinCollision = FindObjectOfType<HandleCoinCollision>(); // tham chiếu đến HandleCoinCollision để có thể lấy được data current
     }
 
     // Update is called once per frame
@@ -19,9 +25,20 @@ public class HandleVictory : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !isGameWon)
         {
-            //gameWinUI.SetActive(true);
+            isGameWon = true;
+            gameWinUI.SetActive(true);
+
+            if (handleCoinCollision != null)
+            {
+                PlayerPrefs.SetInt("saveCoin", handleCoinCollision.Coin);
+                PlayerPrefs.Save();
+            }
+            else
+            {
+                Debug.LogWarning("HandleCollision component not found!");
+            }
         }
     }
 }
