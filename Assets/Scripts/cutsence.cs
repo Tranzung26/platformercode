e1c0bcfc3945853f7bb099fab7df60885c8fd60d
TVQD <94004936+Tranzung26@users.noreply.github.com>
2024-11-08 03:04:43 +0700

    Add Cut Scence,Fixe Boss
    Add Cut Scence,Fixe Boss

---
+++ Assets/Scripts/cutsence.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutsence : MonoBehaviour
+{
+    public GameObject mainMenuCanvas;  // Canvas của menu chính
+    public GameObject cutSceneCanvas;  // Canvas của CutScene
+
+    void Start()
+    {
+        mainMenuCanvas.SetActive(false);
+        cutSceneCanvas.SetActive(true);
+
+        StartCoroutine(PlayCutSceneAndReturnToMenu());
+    }
+
+    private IEnumerator PlayCutSceneAndReturnToMenu()
+    {
+        yield return new WaitForSeconds(5f);
+
+        ReturnToMenu();
+    }
+
+    public void ReturnToMenu()
+    {
+        cutSceneCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(true);
+    }
+}
