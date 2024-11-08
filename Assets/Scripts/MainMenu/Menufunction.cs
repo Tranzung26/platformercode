8e24b0314cb46c0a6310b08c815afabfd8ff3f31
lequangtrieu <trieulqde160447@gmail.com>
2024-11-08 14:44:34 +0700

    update source latest

--- Assets/Scripts/MainMenu/Menufunction.cs
+++ Assets/Scripts/MainMenu/Menufunction.cs
@@ -1,13 +1,28 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     private int currentMapLevel;
     public GameObject messageUI;
+
+    public Button level2Button;
+    public Button level3Button;
+
     void Start()
     {
         currentMapLevel = PlayerPrefs.GetInt("CurrentMapLevel");
+
+        //Kiểm tra nếu currentMapLevel< 2 thì disabled nút Level 2
+        if (currentMapLevel < 2)
+        {
+            level2Button.interactable = false;
+        }
+        if (currentMapLevel < 3)
+        {
+            level3Button.interactable = false;
+        }
     }
     public void Level1()
     {
@@ -23,7 +38,8 @@ public class Menu : MonoBehaviour
 
     public void Level2()
     {
-        SceneManager.LoadScene("DemoMap3");
+        if (currentMapLevel > 2) messageUI.SetActive(true);
+        if (currentMapLevel == 2) SceneManager.LoadScene("DemoMap3");
     }
 
     public void Level3()
